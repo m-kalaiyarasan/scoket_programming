da4030ee0728ccbea9a68694eb3fad0900c59799
kalaiyarasan.offl.2004@gmail.com <kalaiyarasan.offl.2004@gmail.com>
2025-04-21 09:15:44 +0000

    server with serve miltiple files automatically in server_with_files.py sample in using_thread.py

--- server_with_files.py
+++ server_with_files.py
@@ -1,8 +1,17 @@
 import socket as s
 from threading import Thread
+import mimetypes as ty
 
 HOST = '0.0.0.0'
 PORT = 4558
+
+Http_resp_temp = """
+HTTP/1.1 200 OK
+Content-Type: {type}
+Content-Length: {length}
+
+{body}
+"""
 
 class ConnectionThread(Thread):
     def __init__(self,conn,addr):
@@ -11,12 +20,23 @@ class ConnectionThread(Thread):
         self.addr = addr
         print("Thread {}:{} Started".format(addr[0],addr[1]))
     def run(self):
-        self.conn.sendall("welcome to kalai's Server".encode())
+       
         data = self.conn.recv(1024)
-        while data:
+        path='htdocs/'
+        file = data.decode()
+        file = file.split(" ")[1]
+        if(file == "/"):
+            file = 'index.html'
+        file1 = path+file
+        # print(file)
+        with open(file1, 'r') as fi:
+            body = fi.read()            
+        # print(ty.guess_type(file)[0])
+        self.conn.sendall(Http_resp_temp.format(type=ty.guess_type(file)[0],length = len(body)+50,body = body).encode())
+        # while data:
             
-            print("{}: ".format(self.addr[0]) + data.decode(), end="")
-            data = self.conn.recv(1024)
+        #     print("{}: ".format(self.addr[0]) + data.decode(), end="")
+        #     data = self.conn.recv(1024)
             
 
 sck = s.socket(s.AF_INET,s.SOCK_STREAM)
